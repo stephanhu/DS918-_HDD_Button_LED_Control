47ff454ac62d4f87393336e2914017c288286199
stephan <stephan.hueltl@gmx.de>
2018-10-20 00:07:51 +0200

    fix to encode ascii to bytes

--- buttons_leds.py
+++ buttons_leds.py
@@ -7,86 +7,86 @@ ser.close()
 
 def beep_short():
     ser.open()
-    ser.write('2')
+    ser.write("2".encode())
     ser.close()
 
 
 def beep_long():
     ser.open()
-    ser.write('3')
+    ser.write("3".encode())
     ser.close()
 
 
 def power_led_on():
     ser.open()
-    ser.write('4')
+    ser.write("4".encode())
     ser.close()
 
 
 def power_led_flashing():
     ser.open()
-    ser.write('5')
+    ser.write("5".encode())
     ser.close()
 
 
 def power_led_off():
     ser.open()
-    ser.write('6')
+    ser.write("6".encode())
     ser.close()
 
 
 def status_led_off():
     ser.open()
-    ser.write('7')
+    ser.write("7".encode())
     ser.close()
 
 
 def status_green_led_on():
     ser.open()
-    ser.write('8')
+    ser.write("8".encode())
     ser.close()
 
 
 def status_green_led_flashing():
     ser.open()
-    ser.write('9')
+    ser.write("9".encode())
     ser.close()
 
 
 def status_orange_led_on():
     ser.open()
-    ser.write(':')
+    ser.write(":".encode())
     ser.close()
 
 
 def status_orange_led_flashing():
     ser.open()
-    ser.write(';')
+    ser.write(";".encode())
     ser.close()
 
 
 # copy led is on the pcb of DS918+
 def copy_led_off():
     ser.open()
-    ser.write('@')
+    ser.write("@".encode())
     ser.close()
 
 
 def copy_green_led_on():
     ser.open()
-    ser.write('A')
+    ser.write("A".encode())
     ser.close()
 
 
 def copy_green_led_flashing():
     ser.open()
-    ser.write('B')
+    ser.write("B".encode())
     ser.close()
 
 
 def reset():
     ser.open()
-    ser.write('C')
+    ser.write("C".encode())
     ser.close()
 
 
